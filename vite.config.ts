be6158e396c8e7e08846b2e0963f0584b8852f4e
Shubhrangshu Debsark <shrubsdaone@gmail.com>
2025-07-09 20:11:13 -0400

    vite config edited

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "https://shubsds.github.io/postful-social-spark/",
   server: {
     host: "::",
     port: 8080,
